d84755240b571ec141d19754f702ad8fb4998761
art0007i <art0007i@gmail.com>
2023-11-20 13:17:46 +0100

    resonite

--- ImportCompactor/ImportCompactor.cs
+++ ImportCompactor/ImportCompactor.cs
@@ -1,25 +1,24 @@
 using HarmonyLib;
-using NeosModLoader;
+using ResoniteModLoader;
 using System;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using FrooxEngine;
-using FrooxEngine.LogiX.Math.Time;
-using BaseX;
+using Elements.Core;
 
 namespace ImportCompactor
 {
-    public class ImportCompactor : NeosMod
+    public class ImportCompactor : ResoniteMod
     {
         public override string Name => "ImportCompactor";
         public override string Author => "art0007i";
-        public override string Version => "1.0.0";
+        public override string Version => "2.0.0";
         public override string Link => "https://github.com/art0007i/ImportCompactor/";
 
         [AutoRegisterConfigKey]
-        public static ModConfigurationKey<float> KEY_SCALE_FACTOR = new("scale_factor", "Multiplies the position of each import by this value.", () => 0.25f, false, (v)=> v!=0);
+        public static ModConfigurationKey<float> KEY_SCALE_FACTOR = new("scale_factor", "Multiplies the position of each import by this value.", () => 0.75f, false, (v)=> v!=0);
         public static ModConfiguration config;
         public override void OnEngineInit()
         {
